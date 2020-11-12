d4f565a675047ac14fce2e91ca4028b06a17cca1
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-11-11 22:59:12 -0700

    Changed const to var in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection;
-if (process.env.JAWSDB_URL){
+var connection;
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{ mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "lionheart56!",
-  database: "burgers_db"
-});
+} else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "lionheart56!",
+    database: "burgers_db"
+  });
 };
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
